80d00291ec62d5e315ca691fecdf8bf6f7899d05
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-24 17:09:49 +0700

    feat: раздача последнего билда с авторизацией на localhost:4000

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path')
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
     console.log('server started')
